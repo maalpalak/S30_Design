621a193dbbc94e16abad5186b0c8c07d0e81b3e4
maalpalak <90577849+maalpalak@users.noreply.github.com>
2021-09-15 19:07:02 -0400

    Update Design-2 _HashSet.java

--- Design-2 _HashSet.java
+++ Design-2 _HashSet.java
@@ -3,7 +3,7 @@
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
-
+//
 import java.util.*;
 //I have declared size for hash box which is max_val and array size
 //one parent list
